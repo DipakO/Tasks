0fb79cc8290f46ea2809e0aae217b3effb17d299
Dipak Ohol <dipakohol070@gmail.com>
2022-09-01 23:49:41 +0530

    react projects

---
+++ react JS/first-program/src/App.js
@@ -0,0 +1,22 @@
+
+import "./App.css";
+import Box from "./Box";
+const App = () => {
+    return (
+        <>
+
+        <h1>Hello from app.js</h1>
+
+        <Box />    
+        <Box />    
+        <Box />    
+        <Box />     
+        <Box />     
+        <Box />     
+        <Box />     
+        <Box />     
+        </>
+    )
+}
+
+export default App;
---
+++ react JS/first-program/src/Box.js
@@ -0,0 +1,10 @@
+const Box = () =>{
+  return(
+    <span className="box">
+    Below Is The Special Content For This Card
+    <botton id="readmore">ReadMore</botton>
+</span>
+  );
+}
+
+export default Box
